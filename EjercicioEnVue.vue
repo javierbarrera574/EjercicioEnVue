9798cc0a317b3d45d79531ad09d071aab1ceedd3
javierbarrera574 <82546878+javierbarrera574@users.noreply.github.com>
2023-05-18 22:36:07 -0300

    Add files via upload

---
+++ EjercicioEnVue.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar" :disabled="contador === 0">Decrementar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    decrementar() {
+      if (this.contador > 0) {
+        this.contador--;
+      }
+    },
+  },
+};
+</script>
